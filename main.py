29c83b38b5f1fcb266004a2dd934dda4c0a652ce
John Canty <jtcanty117@berkeley.edu>
2017-09-05 19:04:27 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import os
+from PyQt4 import QtGui, QtCore
+
+def main():
+
+
+if __name__ == '__main__':
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name = 'fionaviewer',
+      version = '0.1.0',
+      author = 'John Canty',
+      author_email = 'jtcanty117@berkeley.edu',
+      description = ('A PyQT application for analyzing single-molecule time series signals with discrete steps.'),
+      license = 'MIT',
+      packages = ['src'],
+      install_requires = ['numpy','matplotlib'],
+      include_package_data = True,
+      zip_safe = False
+      url = 'http://github.com/jtcanty/py-fionaviewer
+
+      classifiers = ['Development Status :: 2 - Pre-Alpha',
+                     'Environment :: X11 Applications :: Qt',
+                     'Intended Audience :: Science/Research',
+                     'License :: OSI Approved :: MIT License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2.7',
+                     'Topic :: Multimedia :: Graphics :: Viewers',
+                     'Topic :: Scientific/Engineering :: Physics',
+                     'Topic :: Scientific/Engineering :: Visualization'])
+
+
+
