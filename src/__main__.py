2e9c34a8a58eaea1191dcea7361f29d5be4160d4
John Canty <jtcanty117@gmail.com>
2017-09-11 16:05:06 -0700

    Update MainApplication.py

--- src/__main__.py
+++ src/__main__.py
@@ -8,14 +8,11 @@ from widgets.MainApplication import MainWindow
 
 
 def main():
-
     w = MainWindow()
-
     w.show()
     return app.exec_()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     main()
     sys.exit(0)
--- src/widgets/MainApplication.py
+++ src/widgets/MainApplication.py
@@ -1,19 +1,52 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QMainWindow
+
+import sys
+from pyqtgraph.dockarea import DockArea
+from PyQt5 import QtCore,QtGui
+import pyqtgraph.console
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import numpy as np
 
 class MainWindow(QMainWindow):
-    '''Primary application interface'''
 
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.dock_area = DockArea()
+        self.setCentralWidget(self.dock_area)
+        self.resize(1000,500)
+        self.setWindowTitle('py-fionaviewer')
 
-    def initUI(self):
-        self.statusBar().showMessage('Ready')
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Statusbar')
-        self.show()
+        self.createDocks()
 
+    def createDocks(self):
+        '''Create all the dock widgets and add to DockArea'''
 
+        # Create widgets
+        self.fit = Fit()
+        self.filter = Filter()
+        self.file_io = FileIO()
+        self.trace_plotter = PlotTrace()
+        self.dock_display_settings = DisplaySettings()
+
+        # Add widgets to doc
+        self.dock_fit = Dock('Fit Signal',widget = self.fit)
+        self.dock_filter = Dock('Filter Methods',widget = self.filter)
+        self.dock_file_io = Dock('File IO',widget = self.file_io)
+        self.dock_trace_plotter = Dock('Signal Plot',widget = self.trace_plotter)
+        self.dock_display_settings = Dock('Display Settings',widget = self.display_settings)
+
+        # Add docks to dock area
+        self.dock_area.addDock(self.dock_fit)
+        self.dock_area.addDock(self.dock_filter)
+        self.dock_area.addDock(self.dock_file_io)
+        self.dock_area.addDock(self.dock_trace_plotter)
+        self.dock_area.addDock(self.dock_display_settings)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
