e83c6ee4eec5c3207cd9b815ab0f27ada0efccbd
John Canty <jtcanty117@gmail.com>
2018-05-10 09:43:13 -0700

    Modifying subclasses

--- fionaviewer/filebrowser.py
+++ fionaviewer/filebrowser.py
@@ -9,18 +9,19 @@ from PyQt5.QtCore import QObject, pyqtSignal
 
 class FileBrowser(FileDialog):
     
+    dataChanged = pyqtSignal(object)
+    
     def __init__(self):
         super(FileBrowser, self).__init__()
+        
         
     def getOpenFile(self):      
         fname, _filter =FileDialog.getOpenFileName(self, 'Open file', 'c:\\',"Text files (*.txt)")
         with open(fname, 'r') as f:
             data = f.readlines()
             data = [int(line.strip()) for line in data]
-
-        data_changed = pyqtSignal(data, name='dataChanged')
-
-    
+ 
+        self.dataChanged.emit(data)
             
     def getNewFile(self):
         print('New File')
