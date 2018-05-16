47382a6256a8ec5704e37f8c1adbded4966802e6
John Canty <jtcanty117@berkeley.edu>
2018-05-15 21:22:03 -0700

    Add visual cursor to plotting widget

--- fionaviewer/plotter.py
+++ fionaviewer/plotter.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyqtgraph import PlotWidget
+import pyqtgraph as pg
+
+from pyqtgraph import PlotWidget, ViewBox
+from PyQt5 import QtCore
 
 class Plotter(PlotWidget):
     '''Widget for plotting data'''
@@ -12,9 +15,36 @@ class Plotter(PlotWidget):
         self.filebrowser = filebrowser
         self.filebrowser.dataChanged.connect(self.handleDataChanged)
         
+        self.region = pg.LinearRegionItem
+        self.region.setZvalue(10)
+        self.addItem(self.region, ignoreBounds=True)
+        
+        self.region.sigRegionChanged.connect(updateRange)
+        self.sigRangeChanged.connect(updateRegion)
+        
+        self.region.setRegion([1000, 2000])
+        
     def handleDataChanged(self, data):
         self.pData.setData(data)
-
+        
+    def crossHair(self):
+        vLine = pg.InfiniteLine(angle=90, movable=False)
+        hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.addItem(vLine, ignoreBounds=True)
+        self.addItem(hLine, ignoreBounds=True)
+        
+    def updateRange(self):
+        self.region.setZValue(10)
+        minX, maxX = self.region.getRegion()
+        self.setXRange(minX, maxX, padding=0)
+        
+    def updateRegion(self,viewRange):
+        rng = viewRange[0]
+        self.region.setRegion(rng)
+        
+        
+    def mouseMoved(evt):
+            
         
            
         
