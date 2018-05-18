0212704572c49dda35791a589e49a29f6c682ad7
John Canty <jtcanty117@gmail.com>
2018-05-17 17:04:18 -0700

    Update mouse cursor

--- fionaviewer/plotter.py
+++ fionaviewer/plotter.py
@@ -15,45 +15,48 @@ class Plotter(PlotWidget):
         self.filebrowser = filebrowser
         self.filebrowser.dataChanged.connect(self.handleDataChanged)
         
-        self.region = pg.LinearRegionItem
-        self.region.setZvalue(10)
+        self.region = pg.LinearRegionItem()
+        self.region.setZValue(10)
         self.addItem(self.region, ignoreBounds=True)
-        
-        self.region.sigRegionChanged.connect(updateRange)
-        self.sigRangeChanged.connect(updateRegion)      
         self.region.setRegion([1000, 2000])
+        
+        self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.addItem(self.vLine, ignoreBounds=True)
+        self.addItem(self.hLine, ignoreBounds=True)
+        
+        self.region.sigRegionChanged.connect(self.updateRegion)
+        self.sigRangeChanged.connect(self.updateRange)
+        
+        self.vb = self.plotItem.vb
+        
+        #proxy = pg.SignalProxy(self.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved())
+        self.scene().sigMouseMoved.connect(self.mouseMoved)
     
-        vb = self.plotItem.vb
-        
-        proxy = pg.SignalProxy(self.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
-        
     def handleDataChanged(self, data):
         self.pData.setData(data)
         
-    def crossHair(self):
-        vLine = pg.InfiniteLine(angle=90, movable=False)
-        hLine = pg.InfiniteLine(angle=0, movable=False)
-        self.addItem(vLine, ignoreBounds=True)
-        self.addItem(hLine, ignoreBounds=True)
-        
-    def updateRange(self):
+    def updateRegion(self):
         self.region.setZValue(10)
         minX, maxX = self.region.getRegion()
         self.setXRange(minX, maxX, padding=0)
         
-    def updateRegion(self,viewRange):
-        rng = viewRange[0]
+    def updateRange(self, viewRange):
+        rng = self.viewRange()[0]
         self.region.setRegion(rng)
-              
-    def mouseMoved(evt):
-        pos = evt[0]
+               
+    def mouseMoved(self, evt):
+        pos = evt
         if self.sceneBoundingRect().contains(pos):
-            mousePoint = vb.mapScenetoView(pos)
+            mousePoint = self.vb.mapSceneToView(pos)
             index = int(mousePoint.x())
-            if index > 0 and index < len(data1):
-                label.setText("<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y1=%0.1f</span>,...   <span style='color: green'>y2=%0.1f</span>" % (mousePoint.x(), data1[index], data2[index]))
-                vLine.setPos(mousePoint.x())
-                hLine.setPos(mousePoint.y())
+            if index > 0 and index < len(self.pData):
+                label.setText("<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y1=%0.1f</span>,...   <span style='color: green'>y2=%0.1f</span>" % (mousePoint.x(), self.pData[index]))
+                self.vLine.setPos(mousePoint.x())
+                self.hLine.setPos(mousePoint.y())
+                
+                
+
             
         
             
